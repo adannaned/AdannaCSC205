918c07ddd4694042b010af6757bb013bf0c8e601
adannaned <adanna.ned@gmail.com>
2021-11-04 14:02:01 +0000

    Week 2 practice

---
+++ ASSIGNMENT1/exercise4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+     char myInitial = 'A';
+     char myName[] = "Adanna Ned-onuoha";
+     printf("Your name is = %s\n", myName);
+     printf("The initial of your firstname is = %c\n", myInitial);
+     return 0;
+     
+}
---
+++ ASSIGNMENT1/exercise5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+     int studentAge;
+     printf("Enter age of Student: ");
+     scanf("%d", &studentAge);
+     printf("Student Age = %d\n", studentAge);
+     return 0;
+     
+
+}
